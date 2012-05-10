13719e5f13c63cbc6f4318ae4bbdf1735df518eb
Josh Johnson <jnjosh@gmail.com>
2012-05-09 22:22:07 -0400

    added xcode project

---
+++ Code/BadMoviePodcastApp/JJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JJAppDelegate.h
+//  BadMoviePodcastApp
+//
+//  Created by Joshua Johnson on 5/9/12.
+//  Copyright (c) 2012 jnjosh.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/BadMoviePodcastApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BadMoviePodcastApp
+//
+//  Created by Joshua Johnson on 5/9/12.
+//  Copyright (c) 2012 jnjosh.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJAppDelegate class]));
+    }
+}
